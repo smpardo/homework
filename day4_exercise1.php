bbd6d2c6c096fd3215b8a306840afca101279662
smpardo <scott.m.pardo@gmail.com>
2012-10-01 17:08:08 -0400

    Day 4 Homework 9/29/2012

---
+++ day4_exercise1.php
@@ -0,0 +1,18 @@
+<?php
+	/* Day 4 														*/
+	/* Excercise 1 													*/
+	/* Submitted by Scott Pardo 									*/
+	/* 10/1/2012													*/
+	/* 																*/
+	/* Write a short PHP program that will print the three lines: 	*/
+	/* -Your Name 													*/
+	/* -Your Birth Date 											*/
+	/* -Your Favorite Color 										*/
+
+	/* Output to screen */
+	print 'My name is Scott Pardo.';
+	print '<br>';
+	print 'I was born on October 22nd 1971.';
+	print '<br>';
+	print 'My favorite color is blue.'
+?>
---
+++ day4_exercise2.php
@@ -0,0 +1,37 @@
+<?php
+	/* Day 4 														*/	
+	/* Excercise 2 													*/
+	/* Submitted by Scott Pardo 									*/
+	/* 10/1/2012													*/
+	/* 																*/
+	/* Perform the following calucations using the variables: 		*/
+	/* 		$DaysPerWeek 											*/
+	/* 		$HoursPerDay 											*/
+	/* 		$MinutesPerHour 										*/
+	/*	1. Calculate the number of minutes in a week,  on Earth		*/
+	/*	2. Calculate the number of minutes in a week,  in the 		*/
+	/*     Bilky Way, a near parallel galaxy, if there are 26,		*/ 
+	/*     hours in a day there?									*/ 
+
+	/* Define variables */
+	$DaysPerWeek = 7;
+	$HoursPerDay = 24;
+	$MinutesPerHour = 60;
+	
+	/* Calculate minutes per week for earth*/
+	$MinutesPerWeek = $DaysPerWeek * ($HoursPerDay * $MinutesPerHour);
+		
+	/* Output calculation */
+	print "There are $MinutesPerWeek minutes in one Earth week.";
+	Print '<br>';
+	
+	/* redefine hours per day for Bilky Way */
+	$HoursPerDay = 26;
+	
+	/* Calculate minutes per week for Bilky Way*/
+	$MinutesPerWeek = $DaysPerWeek * ($HoursPerDay * $MinutesPerHour);
+
+	/* Output calculation */
+	print "There are $MinutesPerWeek minutes in one week in the Bilky Way.";
+	Print '<br>';
+?>
