ea650fd32c2d0b766288073e975fc594160cfdd6
smpardo <scott.m.pardo@gmail.com>
2012-10-21 00:45:09 -0400

    Day6 Homework 10/20/2012

---
+++ day6_exercise5.php
@@ -0,0 +1,165 @@
+<!-- Day 6 														
+	 Excercise 5 													
+	 Submitted by Scott Pardo 									
+	 10/19/2012													
+	 															
+	 A store is having a sale.  They are giving the folloiwing discounts:
+	 	10% off purchases of $10 or less.
+	 	20% off purchases of greater than $10.
+	 Write a program to display the discount percentage and final price.
+	 	Use a variable for the item's cost and run multiple cases to test.	 	
+	 Use a form to capture the item's cost, output the discount and the discounted price
+	 and any other information you want.
+	 Form Fields Input:
+	 		list_price - User entered cost of item input object
+	 
+	 Variables:
+	 		$ItemCost - cost of item.
+	 		$FinalPrice - Final cost of item.
+	 		$DiscountTotal - Total amount of discount.
+	 		$DiscountPercent - percentage of discount						
+-->
+<! DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta>
+		<title>Discount Calculator</title>
+		<style type="text/css">
+			/* global text font style */
+			body {
+				font-family: Verdana, Arial, Helvetica, sans-serif;
+			}
+			/* global page style */
+			#container {
+				border-radius: 15px;
+				border: 1px solid black;
+				width: 350px;	
+				padding: 10px;	
+				margin: 0px auto 0px auto;
+			}
+			/* global style for table*/
+			table {
+				border: 0px;
+			}				
+			/* style for column header cells */
+			.colhead {
+				background-color: black;
+				color: white;
+				height: 40px;				
+				text-align: center;
+				font-weight: bold;
+				border-right: 1px solid white;
+				border-left: 1px solid white;
+				border-radius: 15px 15px 0px 0px;	
+				padding: 0px;	
+				margin: 0px;		
+			}
+			/* style for table cells */
+			td {
+				border: 1px solid black;
+				width: 100px;
+				border-radius: 0px 0px 15px 15px;			
+				text-align: right;
+				padding: 5px;
+				margin: 0px;
+			}		
+			/* style for back "button"  */
+			.back {
+				border: 1px solid black;
+				width: 150px;
+				/*height: 10px;*/
+				border-radius: 5px 5px 5px 5px;			
+				padding: 0px;
+				margin: 0px;
+			}			
+			.back a {
+				padding: 10px;
+				font-size: 85%;
+				font-weight: normal;
+				color: black;
+				text-decoration: none;
+				text-align: center;	
+			}
+			
+			/* style for footer */	
+			footer {
+				border-spacing: 0px;
+				float: left;
+				width: 100%;
+			}
+		</style>
+	
+	</head>
+	<body>
+		<section id="container">
+			<header>
+				<h1>Discount Calculator</h1>
+			</header>
+			<!--  -->
+			<?php
+				//instanciate variables to hold multiplication result
+	 			$ItemCost = $_POST["list_price"];	//cost of item from form.
+	 			$DiscountPercent = 0;	//percentage of discount		
+	 			$DiscountTotal = 0;		//total discount amount.				
+	 			$FinalCost = 0;			//final price of item including discount.
+	 			
+	 			//first validate the entry and give a good value
+	 			if ($ItemCost < 0 || is_numeric($ItemCost) == False){
+	 				$ItemCost = 10.50;
+	 				print 'Invalid Entry.  Item Cost must be a number and be positive.  Default value of $10.50 substituted for kicks.';
+	 			}
+				
+	 			//print out the opening div tag
+				print '<table>';
+					
+				//print out the top row of table
+				print '<tr>';
+				print '<td class="colhead">Item Cost</td>';
+				print '<td class="colhead">Discount %</td>';				
+				print '<td class="colhead">Discount Amount</td>';
+				print '<td class="colhead">Final Cost</td>';
+				print '</tr>';
+
+		
+				//check the item cost to see if it is above or below the threshold value
+				if ($ItemCost <= 10){
+					//this should be a 10% discount
+					$DiscountPercent = .10;
+				}else{
+					//this should be a 20% discount	
+					$DiscountPercent = .20;											
+				}
+					
+				//perform the arithmetic and format the output to 2 decimal places
+				$DiscountTotal = money_format('%.2n', $ItemCost * $DiscountPercent) ;
+				$FinalCost = money_format('%.2n',$ItemCost - $DiscountTotal);
+					
+				//open the row for output
+				print '<tr>';
+				//print the result within the table cell tags
+				print '<td> $' . money_format('%.2n',$ItemCost) . '</td>';
+				print '<td> ' . ($DiscountPercent * 100) . '% </td>' ;
+				print '<td> $' . $DiscountTotal . '</td>' ;
+				print '<td> $' . $FinalCost . '&nbsp;</td>';
+					
+				//close the row
+				print '</tr>';
+				//print out the closing div tag
+				print '</table>';
+				
+				//print a space
+				print '<br>';
+				
+				//give the user a link to go back and try again
+				print '<div class="back">';
+				print '<a  href="day6_exercise5.html">Return to Entry Form </a>';
+				print '</div>';
+				
+			?>
+		
+			<footer>
+				<p>Scott Pardo: Day6 Exercise 5. Discount Calculator with Form Entry.</p>
+			</footer>
+		</section>
+	</body>
+</html>
---
+++ day6_exercise6.php
@@ -0,0 +1,145 @@
+<!-- Day 5 														
+	 Excercise 4 													
+	 Submitted by Scott Pardo 									
+	 10/7/2012													
+	 															
+	 Produce a multiplication table using loops.
+	 Use a form to capture the number whose times table will be displayed.
+	 Optional entry of lower an dupper bound for times table. 
+	 Form object names:
+	 		number_entered 	- User entered number for times table.
+	 		lower_bound 	- User entered lower boundary for multiplication.
+	 		upper_bound 	- User entered upper boundary for multiplication.	
+	 Variables:
+	 		$Result - calculation result
+	 		$InputNumber
+	 		$LowerBound
+	 		$UpperBound						
+	 		$i / $j - loop iterators 									
+-->
+<! DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta>
+		<title>Multiplication Tables Creator</title>
+		<style type="text/css">
+			/* global text font style */
+			body {
+				font-family: Verdana, Arial, Helvetica, sans-serif;
+			}
+			/* global table style */
+			#container {
+				border-radius: 15px;
+				border: 1px solid black;
+				width: 400px;	
+				padding: 10px;	
+				margin: 0px auto 0px auto;
+			}
+			/* style for errors */		
+			.error {
+				font-size: 85%;
+				color: red;
+			}		
+			/* style for back "button"  */
+			.back {
+				border: 1px solid black;
+				width: 150px;
+				/*height: 10px;*/
+				border-radius: 5px 5px 5px 5px;			
+				padding: 0px;
+				margin: 0px;
+			}			
+			.back a {
+				padding: 10px;
+				font-size: 85%;
+				font-weight: normal;
+				color: black;
+				text-decoration: none;
+				text-align: center;	
+			}
+			/* style for text */		
+			div p {
+				font-size: 110%;
+				color: black;
+				font-weight: bold;
+			}			
+			/* style for footer */		
+			footer {
+				border-spacing: 0px;
+				float: left;
+				width: 100%;
+			}			
+		</style>
+	</head>
+	<body>
+		<section id="container">
+			<header>
+				<h1>Multiplication Table Creator</h1>
+			</header>
+
+			<?php 
+				//grab the data from the form variables
+				$InputNumber = $_POST["number_entered"];	//Number for Multiplication table from form.	
+				$LowerBound = $_POST["lower_bound"];	//Number for Multiplication table from form.
+				$UpperBound = $_POST["upper_bound"];	//Number for Multiplication table from form.
+
+				//first validate the entry and give a good value if not logical
+				if ($InputNumber < 0 || is_numeric($InputNumber) == False){
+					$InputNumber = 5;
+					print '<div class="error">';
+					print 'Invalid value entrered for multiplier, 5 substituted.';
+					print '</div>';
+		 		}
+				//Now check the lower bound.  If not a valid number subsitute  0
+				if ($LowerBound < 0 || is_numeric($LowerBound) == False){
+					$LowerBound = 0;
+					print '<div class="error">';
+					print 'Invalid value entrered for Lower Bound, 0 substituted.';
+					print '</div>';
+				}
+				//Finally check the upper bound if not valid use 10.  If valid but less than
+				//lower bound use lower bound + 10
+				if ($UpperBound < 0 || is_numeric($UpperBound) == False){
+					$UpperBound = 10;
+					print '<div class="error">';
+					print 'Invalid value entrered for Upper Bound, 10 substituted.';
+					print '</div>';
+				}elseif ($UpperBound < $LowerBound) {
+					$UpperBound = $LowerBound + 10;
+					print '<div class="error">';
+					print 'Upper bound cannot be less than Lower Bound, ' . $UpperBound . ' substituted.';
+					print '</div>';					
+				}				
+				
+				print '<div>';
+				print '<p>Multiplication Table for ' . $InputNumber . ', from ' . $LowerBound . ' to ' . $UpperBound . '. </p>';
+				print '</div>';				
+				
+				//PHP code to follow that outouts the 'guts' of the table 
+				//instanciate variable to hold multiplication result
+				$Result = 0;
+				
+				//main loop to iterate through numbers 1 through 10				
+				for ($i = $LowerBound; $i <= $UpperBound; $i++) {
+
+					//perform the arithmetic
+					$Result = $i * $InputNumber;
+						
+					//print the result for each calculation
+					print $InputNumber . ' X ' . $i . ' = ' . $Result . '<br>';
+				} 
+				
+				print '<br>';
+				
+				//give the user a link to go back and try again
+				print '<div class="back">';
+				print '<a  href="day6_exercise6.html">Return to Entry Form </a>';
+				print '</div>';
+				
+			?>
+			<footer>
+				<p>Scott Pardo: Day6 Exercise 6. Multiplication Table Creator with Form Entry.</p>
+			</footer>
+		</section>
+	</body>
+</html>
